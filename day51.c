e7468e8771e05ebbbcc15378c4725bba3673ada7
TiyaSingh-14 <singhtiya1427@gmail.com>
2026-05-03 17:24:41 +0530

    day 51

---
+++ day51.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node
+struct TreeNode {
+    int val;
+    struct TreeNode *left, *right;
+};
+
+// Create node
+struct TreeNode* newNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+// Insert into BST
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (!root) return newNode(val);
+
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Find LCA
+struct TreeNode* LCA(struct TreeNode* root, int p, int q) {
+    while (root) {
+        if (p < root->val && q < root->val)
+            root = root->left;
+        else if (p > root->val && q > root->val)
+            root = root->right;
+        else
+            return root; // split point
+    }
+    return NULL;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct TreeNode* root = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        root = insert(root, x);
+    }
+
+    int p, q;
+    scanf("%d %d", &p, &q);
+
+    struct TreeNode* ans = LCA(root, p, q);
+
+    if (ans)
+        printf("%d\n", ans->val);
+
+    return 0;
+}
