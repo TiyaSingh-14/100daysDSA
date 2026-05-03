7f0de5f56d496d700285df0edec11078333d7974
TiyaSingh-14 <singhtiya1427@gmail.com>
2026-05-03 23:27:02 +0530

    day 76

---
+++ day76.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int adj[MAX][MAX];
+int visited[MAX];
+
+void dfs(int node, int n) {
+    visited[node] = 1;
+
+    for(int i = 1; i <= n; i++) {
+        if(adj[node][i] && !visited[i]) {
+            dfs(i, n);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    // Initialize adjacency matrix
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            adj[i][j] = 0;
+        }
+    }
+
+    // Input edges
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        scanf("%d %d", &u, &v);
+        adj[u][v] = adj[v][u] = 1;
+    }
+
+    // Initialize visited
+    for(int i = 1; i <= n; i++) {
+        visited[i] = 0;
+    }
+
+    int components = 0;
+
+    for(int i = 1; i <= n; i++) {
+        if(!visited[i]) {
+            dfs(i, n);
+            components++;
+        }
+    }
+
+    printf("%d\n", components);
+
+    return 0;
+}
